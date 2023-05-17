b8ae5dc8d5cd572cb32082c4145c02b54fcecc2e
Kartik Sharma <80048212+theinterestingdev@users.noreply.github.com>
2023-05-17 16:09:20 +0530

    validation update (needs to be edited)

--- Validation/javascript.js
+++ Validation/javascript.js
@@ -1,25 +1,47 @@
-
-function valid()
-{
-
+function valid() {
     var uname = document.getElementById("uname");
-    var uname = document.getElementById("uname_err");
-    var uname = document.getElementById("pass1");
-    var uname = document.getElementById("pass2");
-    if (uname.value=="")
+    var uname_err = document.getElementById("uname_err");
+    var pass = document.getElementById("pass");
+    var pass_err = document.getElementById("pass_err");
+    var cpass = document.getElementById("cpass");
+    var pass_err_length = document.getElementById("pass_err_length");
+
+    if (uname.value.trim() == "") 
     {
-        alert("Please enter the name")
-        document.getElementById("uname").styleborder='2px red solid';
-        document.getElementById("uname_err").style.visibility='visible';
-        document.getElementById("passwd_err1").style.visibility='visible';
-        document.getElementById("passwd_err2").style.visibility='visible';
+        uname_err.style.visibility = 'visible';
         return false;
-    }
-    
+    } 
     else
     {
-        alert("Successfully submit");
+        uname_err.style.visibility = 'hidden';
     }
-    
 
+    if (pass.value.trim() == "")
+    {
+        pass_err.style.visibility = 'visible';
+        return false;
+    } 
+    else 
+    {
+        pass_err.style.visibility = 'hidden';
+    }
+
+    if (pass.value.length < 6)
+    {
+        pass_err_length.style.visibility = 'visible';
+        return false;
+    }
+    else
+    {
+        pass_err_length.style.visibility = 'hidden';
+    }
+
+    if (pass.value != cpass.value)
+    {
+        alert("Passwords do not match");
+        return false;
+    }
+
+    alert("Successfully submitted");
+    return true;
 }
