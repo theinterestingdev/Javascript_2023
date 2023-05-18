5ec8a2fa061e9c046262e0fb10755eed7dcef24e
Kartik Sharma <80048212+theinterestingdev@users.noreply.github.com>
2023-05-18 15:33:20 +0530

    Date_Object

--- Date_Object/javascript.js
+++ Date_Object/javascript.js
@@ -3,3 +3,6 @@ document.write("<br>"+d.toDateString());
 document.write("<br>"+d.toTimeString());
 document.write("<br>"+d.getDate());
 document.write("<br>"+d.getDay());
+document.write("<br>"+d.getFullYear());
+document.write("<br>"+d.getHours());
+document.write("<br>"+d.getHours()+":"+d.getMinutes()+":"+d.getSeconds());
